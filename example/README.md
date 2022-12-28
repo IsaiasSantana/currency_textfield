90c4375f989e0a2c962c79aaffa8c69bfc965486
Gian <gian_bettega@hotmail.com>
2022-12-28 20:25:32 -0300

    improve example

---
+++ example/README.md
@@ -0,0 +1,3 @@
+# currency_textfield_2_example
+
+Demonstrates how to use the currency_textfield_2 package.
---
+++ example/android/app/src/main/kotlin/edu/hm/krivoj/monthpickerdialogexample/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.com.currencytextfield2example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
